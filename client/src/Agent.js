f2a921b1a9e93843b0a7931e25532fdf4b802e8b
Aimee <awhitaker2@wisc.edu>
2020-11-01 15:03:32 -0600

    add functionality for create, delete, and update

--- client/src/Agent.js
+++ client/src/Agent.js
@@ -9,9 +9,10 @@ class Agent extends React.Component {
       firstName: '',
       middleName: '',
       lastName: '',
-      dob: 0,
+      dob: '',
       heightInInches: 0,
       errors: [],
+      mode: 'Add',
     };
   }
 
@@ -31,52 +32,126 @@ class Agent extends React.Component {
 
   changeHandler = (event) => {
     this.setState({
-        [event.target.name]: event.target.value
+        [event.target.className]: event.target.value
     });
   }
 
-
-  addSubmitHandler = (event) => {
+  
+  editSubmitHandler = (event) => {
     event.preventDefault();
-
-    const {
-      firstName,
-      middleName,
-      lastName,
-      dob,
-      heightInInches,
-    } = this.state;
-
-    fetch('http://localhost:8080/api/agent', {
-      method: 'POST',
+    fetch(`http://localhost:8080/api/agent/${this.state.agentId}`, {
+      method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        firstName,
-        middleName,
-        lastName,
-        dob,
-        heightInInches,
+        agentId: this.state.agentId,
+        firstName: this.state.firstName,
+        middleName: this.state.middleName === '' ? null : this.state.middleName,
+        lastName: this.state.lastName,
+        dob: this.state.dob === '' ? null : this.state.dob,
+        heightInInches: this.state.heightInInches,
       }),
     })
       .then((response) => {
-        if (response.status === 201) {
+        if (response.status === 204) {
           this.setState({
+            agentId: 0,
             firstName: '',
             middleName: '',
             lastName: '',
-            dob: 0,
+            dob: '',
             heightInInches: 0,
             errors: [],
+            mode: 'Add',
           });
           this.getAgents();
         } else if (response.status === 400) {
           response.json().then((data) => this.setState({
             errors: data,
           }));
-        } else {
-          throw new Error(`Unexpected response: ${response}`);
+        } 
+        else if (response.status === 500) {
+          response.json().then(data => console.log(data));
+        }
+        else {
+          console.log("yeah that didn't work");
+        }
+      });
+  }
+
+  addSubmitHandler = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:8080/api/agent', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName: this.state.firstName,
+        middleName: this.state.middleName === '' ? null : this.state.middleName,
+        lastName: this.state.lastName,
+        dob: this.state.dob === '' ? null : this.state.dob,
+        heightInInches: this.state.heightInInches,
+      }),
+    })
+      .then((response) => {
+        if (response.status === 201) {
+          response.json().then(data => console.log(data));
+          this.setState({
+            agentId: 0,
+            firstName: '',
+            middleName: '',
+            lastName: '',
+            dob: '',
+            heightInInches: 0,
+            errors: [],
+            mode: 'Add',
+          });
+          this.getAgents();
+        } else if (response.status === 400) {
+          response.json().then((data) => this.setState({
+            errors: data,
+          }));
+        }
+        else if (response.status === 500) {
+          response.json().then(data => console.log(data));
+        }
+        else {
+          console.log("yeah that didn't work");
+        }
+      });
+  }
+
+  editAgent = (agentId) => {
+    fetch(`http://localhost:8080/api/agent/${agentId}`)
+      .then((response) => response.json())
+      .then(({ agentId, firstName, middleName, lastName, dob, heightInInches }) => {
+        this.setState({
+          agentId,
+          firstName,
+          middleName,
+          lastName,
+          dob,
+          heightInInches,
+          mode: 'Edit',
+        });
+      });
+  }
+
+  deleteAgent = (agentId) => {
+    fetch(`http://localhost:8080/api/agent/${agentId}`, {
+      method: 'DELETE',
+    })
+      .then((response) => {
+        if (response.status === 204) {
+          this.getAgents();
+        }
+        else if (response.status === 500) {
+          response.json().then(data => console.log(data));
+        }
+        else {
+          console.log("yeah that didn't work");
         }
       });
   }
@@ -86,6 +161,7 @@ class Agent extends React.Component {
     const {
       agents,
       errors,
+      mode,
     } = this.state;
 
     return (
@@ -102,25 +178,47 @@ class Agent extends React.Component {
             </div>
           )}
 
+          {mode === 'Add' && (      
+            <form className="form-inline p-3" onSubmit={this.addSubmitHandler}>
+              <div>
+                <input className="firstName" value={this.firstName} onChange={this.changeHandler} placeholder="Enter a first name..." type="text" />
+              </div>
+              <div>
+                <input className="middleName" value={this.middleName} onChange={this.changeHandler} placeholder="Enter a middle name.." type="text" />
+              </div>
+              <div>
+                <input className="lastName" value={this.lastName} onChange={this.changeHandler} placeholder="Enter a last name..." type="text" />
+              </div>
+              <div>
+                <input className="dob" value={this.dob} onChange={this.changeHandler} placeholder="Enter a date of birth" type="text" />
+              </div>
+              <div>
+                <input className="heightInInches" value={this.heightInInches} onChange={this.changeHandler} placeholder="Enter a height in inches..." type="text" />
+              </div>
+              <button className="btn btn-success ml-2" type="submit">Add Agent</button>
+            </form> 
+          )}
 
-         <form className="form-inline p-3" onSubmit={this.addSubmitHandler}>
-            <div>
-              <input className="firstName" value={this.firstName} onChange={this.changeHandler} placeholder="Enter a first name..." type="text" />
-            </div>
-            <div>
-              <input className="middleName" value={this.middleName} onChange={this.changeHandler} placeholder="Enter a middle name.." type="text" />
-            </div>
-            <div>
-              <input className="lastName" value={this.lastName} onChange={this.changeHandler} placeholder="Enter a last name..." type="text" />
-            </div>
-            <div>
-              <input className="dob" value={this.dob} onChange={this.changeHandler} placeholder="Enter a date of birth" type="text" />
-            </div>
-            <div>
-              <input className="heightInInches" value={this.heightInInches} onChange={this.changeHandler} placeholder="Enter a height in inches..." type="text" />
-            </div>
-            <button className="btn btn-success ml-2" type="submit">Add Agent</button>
-          </form> 
+          {mode === 'Edit' && (
+            <form className="form-inline p-3" onSubmit={this.editSubmitHandler}>
+              <div>
+                <input className="firstName" value={this.firstName} onChange={this.changeHandler} placeholder="Enter a first name..." type="text" />
+              </div>
+              <div>
+                <input className="middleName" value={this.middleName} onChange={this.changeHandler} placeholder="Enter a middle name.." type="text" />
+              </div>
+              <div>
+                <input className="lastName" value={this.lastName} onChange={this.changeHandler} placeholder="Enter a last name..." type="text" />
+              </div>
+              <div>
+                <input className="dob" value={this.dob} onChange={this.changeHandler} placeholder="Enter a date of birth" type="text" />
+              </div>
+              <div>
+                <input className="heightInInches" value={this.heightInInches} onChange={this.changeHandler} placeholder="Enter a height in inches..." type="text" />
+              </div>
+              <button className="btn btn-success ml-2" type="submit">Update Agent</button>
+            </form>
+         )}
 
         <table className="table table-dark table-striped table-hover">
           <thead>
@@ -137,6 +235,12 @@ class Agent extends React.Component {
                 <td>{agent.lastName}</td>
                 <td>{agent.dob}</td>
                 <td>{agent.heightInInches}</td>  
+                <td>
+                  <div className="float-right">
+                    <button className="btn btn-sm btn-primary mr-2" type="button" onClick={() => this.editAgent(agent.agentId)}>Edit</button>
+                    <button className="btn btn-sm btn-danger" type="button" onClick={() => this.deleteAgent(agent.agentId)}>Delete</button>
+                  </div>
+                </td>
               </tr>
             ))}
           </tbody>
